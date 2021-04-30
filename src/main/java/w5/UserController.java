949f3426daf03cd1eef5084a7abe15ee0a4ccd3a
MikiKru <michal_kruczkowski@o2.pl>
2021-04-30 12:29:41 +0200

    P2 - lect05 v6

---
+++ src/main/java/w5/UserController.java
@@ -0,0 +1,31 @@
+package w5;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class UserController {
+    private static List<User> users = new ArrayList<>(Arrays.asList(
+            new User(1,"m@m.pl","m", LocalDate.of(2000,4,3), true),
+            new User(2,"e@e.pl","eee", LocalDate.of(2001,1,4), true),
+            new User(3,"r@r.pl","rr", LocalDate.of(2002,12,7), false),
+            new User(4,"q@q.pl","q", LocalDate.of(1987,5,8), true),
+            new User(5,"b@b.pl","bbb", LocalDate.of(1999,2,11), true)
+    ));
+
+    public static void main(String[] args) {
+        System.out.println("Wypisz użytkowników");
+        users.stream().forEach(user -> System.out.println(user));
+        System.out.println("Wypisz tylko użytkowników aktywnych");
+        users.stream().filter(user -> user.isStatus()).forEach(user -> System.out.println(user));
+        System.out.println("Wypisz tylko użytkowników aktywynch posortowanych po dacie urodzenia malejąco");
+        users.stream()
+                .filter(user -> user.isStatus())
+                .sorted(Comparator.comparing(User::getBithDate).reversed())
+                .forEach(user -> System.out.println(user));
+        System.out.println("Wypisz użytkoników posortowanych po loginie od a do z");
+
+    }
+}
