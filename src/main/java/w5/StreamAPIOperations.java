ad93c12acf704f12c4ba422215ccd4b11b580881
MikiKru <michal_kruczkowski@o2.pl>
2021-04-30 11:41:23 +0200

    DT - lect05 v0

---
+++ src/main/java/w5/StreamAPIOperations.java
@@ -0,0 +1,14 @@
+package w5;
+
+public class StreamAPIOperations {
+    public static void main(String[] args) {
+        int [] numbers = {1,2,3,4,5,4,3,2,1,5,6,7,8,7};
+        // wypisz liczby
+        for(int number : numbers) {
+            System.out.print(number + " ");
+        }
+        System.out.println();
+        System.out.println("STREAM API");
+
+    }
+}
