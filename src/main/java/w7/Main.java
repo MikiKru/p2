4ba4374df456c024f9a0acd26f684300b123b99e
MikiKru <michal_kruczkowski@o2.pl>
2021-05-21 12:32:44 +0200

    P2 w7 v3

--- src/main/java/w7/Main.java
+++ src/main/java/w7/Main.java
@@ -1,5 +1,35 @@
 package w7;
 
-public class Main {
+import java.util.Random;
 
+public class Main {
+    public static void main(String[] args) {
+        Account account = new Account(10_000);
+        Thread thIncome = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++){
+                    try {
+                        Thread.currentThread().sleep(1000);
+                        int income = new Random().nextInt(100)*10;
+                        System.out.println("Income: " + income);
+                        account.setSaldo(account.getSaldo() + income);
+                        System.out.println("Saldo: " + account.getSaldo());
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } } } });
+        Thread thOutcome = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 15; i++){
+                    try {
+                        Thread.currentThread().sleep(1300);
+                        int outcome = new Random().nextInt(100)*10;
+                        System.out.println("Outcome: " + outcome);
+                        account.setSaldo(account.getSaldo() - outcome);
+                        System.out.println("Saldo: " + account.getSaldo());
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } } } });
+    }
 }
