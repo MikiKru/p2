191d47c3c5bc202ed1ee811759da33681ad6ea1d
MikiKru <michal_kruczkowski@o2.pl>
2021-05-21 12:26:10 +0200

    P2 w7 v2

---
+++ src/main/java/w7/Account.java
@@ -0,0 +1,14 @@
+package w7;
+
+public class Account {
+    private int saldo;
+    public Account(int saldo) {
+        this.saldo = saldo;
+    }
+    public int getSaldo() {
+        return saldo;
+    }
+    public void setSaldo(int saldo) {
+        this.saldo = saldo;
+    }
+}
